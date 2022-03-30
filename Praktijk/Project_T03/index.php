b58644aa499a925c28c9eebbeb592fa60b37a1dc
zkainee <kainevommeren@gmail.com>
2022-03-30 15:07:55 +0200

    First commit

---
+++ Praktijk/Project_T03/index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Author: Kaine van Ommere & Daan Wolfert
+ * Date: 30-03-2022
+ */
+
+?>
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ICT Academie</title>
+</head>
+    <header>
+
+    </header>
+    <body>
+
+    </body>
+</html>
