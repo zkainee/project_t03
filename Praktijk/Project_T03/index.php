b4bf7126eebe255493217e567f338c85a64427eb
zkainee <kainevommeren@gmail.com>
2022-04-04 11:34:10 +0200

    Added hamburger.png icon

--- Praktijk/Project_T03/index.php
+++ Praktijk/Project_T03/index.php
@@ -5,29 +5,28 @@
  */
 ?>
 <!doctype html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ICT Academie</title>
-    <link href="css/style.css" rel="stylesheet">
-</head>
-    <header>
+    <html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>ICT Academie</title>
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+        <header>
+            <?php
+                include "./include/navigation.php";
+            ?>
+        </header>
+        <body>
+            <h2>
+                lorem ipsum dolor sit amet
+            </h2>
+        </body>
+    <footer>
         <?php
-            include "./include/navigation.php";
+        include "./include/footer.php";
         ?>
-    </header>
-    <body>
-        <h2>
-
-            lorem ipsum dolor sit amet
-        </h2>
-    </body>
-<footer>
-    <?php
-    include "./include/footer.php";
-    ?>
-</footer>
+    </footer>
 </html>
