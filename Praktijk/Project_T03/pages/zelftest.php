adb0fdd73e1aeb54bfdb2d87e49b55cf3dd80181
zkainee <kainevommeren@gmail.com>
2022-04-05 11:39:17 +0200

    Fonts added & base zelftest.php html

--- Praktijk/Project_T03/pages/zelftest.php
+++ Praktijk/Project_T03/pages/zelftest.php
@@ -1,1 +1,34 @@
 <?php
+/*
+ * Author: Kaine van Ommeren & Daan Wolfert
+ * Date: 05-04-2022
+ * File: zelftest.php
+ */
+?>
+<!doctype html>
+    <html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>ICT Academie</title>
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <header>
+        <?php
+        include "./include/navigation.php";
+        ?>
+    </header>
+    <body>
+    <h2>
+        lorem ipsum dolor sit amet
+    </h2>
+    </body>
+    <footer>
+        <?php
+        include "./include/footer.php";
+        ?>
+    </footer>
+</html>
+
