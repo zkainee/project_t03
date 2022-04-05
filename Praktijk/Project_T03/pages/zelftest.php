97e878fe304efca6b2056057223d502af19dc4d0
zkainee <kainevommeren@gmail.com>
2022-04-05 12:31:23 +0200

    Front-end

--- Praktijk/Project_T03/pages/zelftest.php
+++ Praktijk/Project_T03/pages/zelftest.php
@@ -13,21 +13,22 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>ICT Academie</title>
-        <link href="css/style.css" rel="stylesheet">
+        <link rel="icon" href="https://www.kw1c.nl/favicon-32x32.png?v=kw1cweb3">
+        <link href="../css/style.css" rel="stylesheet">
     </head>
     <header>
         <?php
-        include "./include/navigation.php";
+        include "../include/navigation.php";
         ?>
     </header>
     <body>
-    <h2>
-        lorem ipsum dolor sit amet
-    </h2>
+        <h2>
+            lorem ipsum dolor sit amet
+        </h2>
     </body>
     <footer>
         <?php
-        include "./include/footer.php";
+        include "../include/footer.php";
         ?>
     </footer>
 </html>
